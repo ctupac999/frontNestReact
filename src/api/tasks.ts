58fb2309e60319f38978ca82d893bf68a81dd57d
ctupac999 <cristiands9999@gmail.com>
2024-09-27 16:05:09 +0200

    ok

--- src/api/tasks.ts
+++ src/api/tasks.ts
@@ -1,6 +1,6 @@
 import {CreateTask, UpdateTask} from '../interfaces/task.interface'
 
-const API= 'http://localhost:3000/api'
+const API= 'http://localhost:10000/api'
 
 export const createTaskRequest = (task: CreateTask) => 
     fetch (`${API}/tasks`,{
